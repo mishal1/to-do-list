446657a141e57042f2fad7e0351e75fc8097e98b
Mishal Islam <mishal.islam01@gmail.com>
2014-12-24 13:43:08 +0000

    binded alerts when a box is ticked or not ticked

--- public/js/task.js
+++ public/js/task.js
@@ -1,26 +1,48 @@
-var addTaskButton = document.getElementById('addTaskButton')
-var incompleteTasks = document.getElementById('incompleteTasks')
-var taskInput = document.getElementById('userInput')
+var taskInput = document.getElementById("userInput"); //new-task
+var addTaskButton = document.getElementById('addTaskButton'); //first button
+var incompleteTasks = document.getElementById("incompleteTasks"); //incomplete-tasks
+var completedTasks= document.getElementById("completedTasks"); //completed-tasks
 
-var newTaskElement = function(taskString) {
-  var listItem = document.createElement("li")
-  var label = document.createElement("label")
-  var checkbox = document.createElement("input")
-  label.innerText = taskString
-  checkbox.type = "checkbox"
-  checkbox.className = "checkbox"
-  listItem.appendChild(checkbox)
-  listItem.appendChild(label)
+
+var NewTaskElement = function(taskString) {
+  var listItem = document.createElement("li");
+  var checkBox = document.createElement("input"); // checkbox
+  var label = document.createElement("label");
+  checkBox.type = "checkbox";
+  label.innerText = taskString;
+  listItem.appendChild(checkBox);
+  listItem.appendChild(label);
   return listItem;
-};
+}
 
 var addTask = function() {
-  var listItem = newTaskElement(taskInput.value)
-  incompleteTasks.appendChild(listItem)
-};
+  var listItem = NewTaskElement(taskInput.value);
+  incompleteTasks.appendChild(listItem);
+  bindTaskEvents(listItem, taskCompleted);
+  taskInput.value = "";
+}
 
+var taskCompleted = function() {
+  alert("Task complete...");
+}
 
-addTaskButton.addEventListener('click', function(){
-  event.preventDefault();
-  addTask()        
-});
+var taskIncomplete = function() {
+  alert("Task incomplete...");
+}
+
+var bindTaskEvents = function(taskListItem, checkBoxEventHandler) {
+  var checkBox = taskListItem.querySelector("input[type=checkbox]");
+  var editButton = taskListItem.querySelector("button.edit");
+  checkBox.onchange = checkBoxEventHandler;
+}
+
+addTaskButton.addEventListener("click", addTask);
+
+for(var i = 0; i < incompleteTasks.children.length; i++) {
+  bindTaskEvents(incompleteTasks.children[i], taskCompleted);
+}
+
+//cycle over completedTasksHolder ul list items
+for(var i = 0; i < completedTasks.children.length; i++) {
+  bindTaskEvents(completedTasks.children[i], taskIncomplete);
+}
